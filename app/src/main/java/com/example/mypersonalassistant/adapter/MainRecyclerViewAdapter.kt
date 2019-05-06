e5ab4c47265e7a9ec3dad6536ac95a63594b6c07
David Aleu Mosegui <david.aleu.16@gmail.com>
2019-05-06 21:05:51 +0200

    calendar view added

--- app/src/main/java/com/example/mypersonalassistant/adapter/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/mypersonalassistant/adapter/MainRecyclerViewAdapter.kt
@@ -13,55 +13,64 @@ import com.example.mypersonalassistant.model.WeatherModel
 import java.util.*
 
 
-class MainRecyclerViewAdapter(val list: ArrayList<WeatherModel>) : RecyclerView.Adapter<MainRecyclerViewAdapter.MainInfoViewHolder>(){
+
+
+class MainRecyclerViewAdapter(val list: ArrayList<WeatherModel>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
     @SuppressLint("SetTextI18n")
-    override fun onBindViewHolder(holder: MainInfoViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val weather = list[position % list.size]
 
         var day = false
         val currentTime = System.currentTimeMillis()/1000
         if(currentTime>weather.sunrise && currentTime<weather.sunset) day = true
 
-        holder.cardTitleViewWeather.text = weather.name
-        if(day) holder.cardImageViewTitleWeather.setImageResource(R.drawable.ic_day)
-        else holder.cardImageViewTitleWeather.setImageResource(R.drawable.ic_night)
+        if(holder.itemViewType==0){
+            var holderWeather: MainInfoViewWeatherHolder = holder as MainInfoViewWeatherHolder
 
-        holder.tempratureText.text = weather.temp.toString() + " ºC"
-        if(weather.temp>10) holder.tempratureImage.setImageResource(R.drawable.ic_hot)
-        else holder.tempratureImage.setImageResource(R.drawable.ic_cold)
+            holderWeather.cardTitleViewWeather.text = weather.name
+            if(day) holderWeather.cardImageViewTitleWeather.setImageResource(R.drawable.ic_day)
+            else holderWeather.cardImageViewTitleWeather.setImageResource(R.drawable.ic_night)
 
-        holder.humidityText.text = weather.humidity.toString()+" %"
-        holder.humidityImage.setImageResource(R.drawable.ic_humidity)
+            holderWeather.tempratureText.text = weather.temp.toString() + " ºC"
+            if(weather.temp>10) holderWeather.tempratureImage.setImageResource(R.drawable.ic_hot)
+            else holderWeather.tempratureImage.setImageResource(R.drawable.ic_cold)
 
-        holder.windText.text = weather.windSpeed.toString() + " km/h"
-        holder.windImage.setImageResource(R.drawable.ic_wind)
+            holderWeather.humidityText.text = weather.humidity.toString()+" %"
+            holderWeather.humidityImage.setImageResource(R.drawable.ic_humidity)
 
-        if(weather.weather=="Clear"){
-            if(day) holder.currentWeatherMainImage.setImageResource(R.drawable.ic_clear_day)
-            else holder.currentWeatherMainImage.setImageResource(R.drawable.ic_clear_day)
-        }
-        else if(weather.weather=="Clouds"){
-            if(weather.description=="few clouds") {
-                if(day) holder.currentWeatherMainImage.setImageResource(R.drawable.ic_cloudy_half_day)
-                else holder.currentWeatherMainImage.setImageResource(R.drawable.ic_cloudy_half_night)
+            holderWeather.windText.text = weather.windSpeed.toString() + " km/h"
+            holderWeather.windImage.setImageResource(R.drawable.ic_wind)
+
+            if(weather.weather=="Clear"){
+                if(day) holderWeather.currentWeatherMainImage.setImageResource(R.drawable.ic_clear_day)
+                else holderWeather.currentWeatherMainImage.setImageResource(R.drawable.ic_clear_day)
             }
-            else holder.currentWeatherMainImage.setImageResource(R.drawable.ic_cloudy)
-        }
-        else if(weather.weather=="Rain" || weather.weather=="Drizzle"){
-            holder.currentWeatherMainImage.setImageResource(R.drawable.ic_rain)
-        }
-        else if(weather.weather=="Thunderstorm"){
-            holder.currentWeatherMainImage.setImageResource(R.drawable.ic_storm)
-        }
-        else if(weather.weather=="Snow"){
-            holder.currentWeatherMainImage.setImageResource(R.drawable.ic_snowy)
-        }
-        else{
-            holder.currentWeatherMainImage.setImageResource(R.drawable.ic_fog)
-        }
+            else if(weather.weather=="Clouds"){
+                if(weather.description=="few clouds") {
+                    if(day) holderWeather.currentWeatherMainImage.setImageResource(R.drawable.ic_cloudy_half_day)
+                    else holderWeather.currentWeatherMainImage.setImageResource(R.drawable.ic_cloudy_half_night)
+                }
+                else holderWeather.currentWeatherMainImage.setImageResource(R.drawable.ic_cloudy)
+            }
+            else if(weather.weather=="Rain" || weather.weather=="Drizzle"){
+                holderWeather.currentWeatherMainImage.setImageResource(R.drawable.ic_rain)
+            }
+            else if(weather.weather=="Thunderstorm"){
+                holderWeather.currentWeatherMainImage.setImageResource(R.drawable.ic_storm)
+            }
+            else if(weather.weather=="Snow"){
+                holderWeather.currentWeatherMainImage.setImageResource(R.drawable.ic_snowy)
+            }
+            else{
+                holderWeather.currentWeatherMainImage.setImageResource(R.drawable.ic_fog)
+            }
 
-        holder.weatherConditionMain.text = "("+weather.description+")"
+            holderWeather.weatherConditionMain.text = "("+weather.description+")"
+        }
+        else {
+            var holderCalendar: MainInfoViewCalendarHolder = holder as MainInfoViewCalendarHolder
+        }
 
     }
 
@@ -70,12 +79,32 @@ class MainRecyclerViewAdapter(val list: ArrayList<WeatherModel>) : RecyclerView.
         else return Integer.MAX_VALUE
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainInfoViewHolder {
-        val itemView = LayoutInflater.from(parent?.context).inflate(R.layout.content_main_card_view_weather,parent,false)
-        return MainInfoViewHolder(itemView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if(viewType==0){
+            val itemView = LayoutInflater.from(parent?.context).inflate(R.layout.content_main_card_view_weather,parent,false)
+            return MainInfoViewWeatherHolder(itemView)
+        }
+        else {
+            val itemView = LayoutInflater.from(parent?.context).inflate(R.layout.content_main_card_view_calendar,parent,false)
+            return MainInfoViewCalendarHolder(itemView)
+        }
     }
 
-    class MainInfoViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    override fun getItemViewType(position: Int): Int {
+        // Just as an example, return 0 or 2 depending on position
+        // Note that unlike in ListView adapters, types don't have to be contiguous
+        return position % 2 * 2
+    }
+
+    class MainInfoViewCalendarHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var cv: CardView
+
+        init {
+            cv = view.findViewById(R.id.calendar_card_view)
+        }
+    }
+
+    class MainInfoViewWeatherHolder(view: View) : RecyclerView.ViewHolder(view){
 
         var cv: CardView
 
